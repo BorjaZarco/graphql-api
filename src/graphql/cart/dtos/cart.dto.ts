d92f7770b0f29a12ba838dd6624a5f8f7eec92a5
Borja Zarco <borja@squaads.com>
2021-07-21 16:37:12 +0100

    improve cart dto

--- src/graphql/cart/dtos/cart.dto.ts
+++ src/graphql/cart/dtos/cart.dto.ts
@@ -1,4 +1,6 @@
-import { Field, ID, InputType, ObjectType } from 'type-graphql';
+import { Field, Float, ID, InputType, ObjectType } from 'type-graphql';
+import { CartStatusEnum } from '../../../types/enums/cart-status.enum';
+import { CartModel } from '../cart.model';
 import { Item } from './item.dto';
 
 @ObjectType()
@@ -9,13 +11,27 @@ export class Cart {
   @Field(() => ID)
   userId!: string;
 
+  @Field()
+  status!: string;
+
+  @Field(() => Float)
+  totalPrice!: number;
+
   @Field(() => [Item])
   items!: Item[];
 
-  constructor(userId: string) {
-    this.id = `${Date.now()}`;
-    this.items = [];
+  constructor(
+    id: string = `${(CartModel.getCarts()?.length || 0) + 1}`,
+    userId: string,
+    items: Item[] = [],
+    status: CartStatusEnum = CartStatusEnum.Pending
+  ) {
+    this.id = id;
     this.userId = userId;
+    this.items = items;
+    this.totalPrice = items.reduce((total, item) => total + (item?.quantity || 0) * (item?.price || 0), 0);
+
+    this.status = status;
   }
 }
 
