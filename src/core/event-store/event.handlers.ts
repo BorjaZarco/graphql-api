c10ff403a5323781b3c5d6df10cd2da7e4b19f56
Borja Zarco <borja@squaads.com>
2021-07-21 16:36:12 +0100

    add item updated handler

--- src/core/event-store/event.handlers.ts
+++ src/core/event-store/event.handlers.ts
@@ -1,5 +1,7 @@
 import { CartCreatedEvent } from '../../graphql/cart/events/cart-created.event';
+import { ItemUpdatedEvent } from '../../graphql/cart/events/item-updated.event';
 import { CartCreatedHandler } from '../../graphql/cart/handlers/cart-created.handler';
+import { ItemUpdatedHandler } from '../../graphql/cart/handlers/item-updated.handler';
 import { IEvent } from './event-message.interface';
 import { EventTypeEnum } from './event-type.enum';
 
@@ -8,8 +10,8 @@ export class EventHandlers {
     switch (event.type) {
       case EventTypeEnum.CartCreated:
         return CartCreatedHandler.handle(event as CartCreatedEvent);
-      case EventTypeEnum.CartUpdated:
-        throw new Error('No handler implemented');
+      case EventTypeEnum.ItemUpdated:
+        return ItemUpdatedHandler.handle(event as ItemUpdatedEvent);
     }
   }
 }
