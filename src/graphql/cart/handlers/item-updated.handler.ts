7606f5c167cda3b13664332e4d232090681022b5
Borja Zarco <borja@squaads.com>
2021-07-21 16:38:05 +0100

    add item updated handler

---
+++ src/graphql/cart/handlers/item-updated.handler.ts
@@ -0,0 +1,19 @@
+import { CartModel } from '../cart.model';
+import { Cart } from '../dtos/cart.dto';
+import { ItemUpdatedEvent } from '../events/item-updated.event';
+
+export class ItemUpdatedHandler {
+  static handle(event: ItemUpdatedEvent) {
+    console.log('Cart items updated!!');
+
+    const cart = CartModel.getCart(event.cartId);
+    const itemIdx = cart.items.findIndex((item) => item.sku === event.item.sku);
+    if (itemIdx !== -1) {
+      cart.items.splice(itemIdx, 1, event.item);
+    } else {
+      cart.items.push(event.item);
+    }
+
+    CartModel.updateCart(new Cart(cart.id, cart.userId, cart.items));
+  }
+}
