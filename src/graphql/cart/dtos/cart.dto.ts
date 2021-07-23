d17d1fe4bb2093261d40e9fc92611aef34ac96c0
Borja Zarco <borja@squaads.com>
2021-07-23 11:55:07 +0100

    remove status from cart

--- src/graphql/cart/dtos/cart.dto.ts
+++ src/graphql/cart/dtos/cart.dto.ts
@@ -1,5 +1,4 @@
 import { Field, Float, ID, InputType, ObjectType } from 'type-graphql';
-import { CartStatusEnum } from '../../../types/enums/cart-status.enum';
 import { Item } from './item.dto';
 
 @ObjectType()
@@ -10,20 +9,16 @@ export class Cart {
   @Field(() => ID)
   userId!: string;
 
-  @Field()
-  status!: string;
-
   @Field(() => Float)
   totalPrice!: number;
 
   @Field(() => [Item])
   items!: Item[];
 
-  constructor(userId: string, items: Item[] = [], status: CartStatusEnum = CartStatusEnum.Pending) {
+  constructor(userId: string, items: Item[] = []) {
     this.userId = userId;
     this.items = items;
     this.totalPrice = items.reduce((total, item) => total + (item?.quantity || 0) * (item?.price || 0), 0);
-    this.status = status;
   }
 }
 
