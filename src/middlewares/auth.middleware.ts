34bac0bfdf5ffd491efb82fe05e02a7249a6edf8
Borja Zarco <borja@squaads.com>
2021-07-24 16:49:52 +0100

    add auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,14 @@
+import { AuthChecker } from 'type-graphql';
+import { AuthService } from '../services/auth.service';
+import { IContext } from '../types/definitions/context';
+
+export class AuthMiddleware {
+  static check: AuthChecker<IContext> = async ({ context }): Promise<boolean> => {
+    const token = context?.req?.headers?.authorization;
+    if (!token) {
+      throw new Error('You must provide an auth token');
+    }
+
+    return await AuthService.verifyToken(token);
+  };
+}
