6b75236719d23db58c8ff08adf4913000fb28d1e
Borja Zarco <borja@squaads.com>
2021-07-25 11:13:30 +0100

    add subsciption support on event store

--- src/core/event-store/event-store.ts
+++ src/core/event-store/event-store.ts
@@ -1,14 +1,24 @@
+import { PubSub } from 'apollo-server-express';
 import { EventEntity } from '../../repostory/mongo/entitites/event.entity';
 import { IEvent } from './event-message.interface';
+import { EventTypeEnum } from './event-type.enum';
 import { EventHandlers } from './event.handlers';
 
-class EventStore {
-  async execute(event: IEvent) {
+const Stream = new PubSub();
+
+export class EventStore {
+  static async execute(event: IEvent) {
     // store the event
     await EventEntity.create(event);
     // call event handler, do not wait for it
     void EventHandlers.handle(event);
   }
-}
 
-export default new EventStore();
+  static notify<T>(eventName: EventTypeEnum, payload: T) {
+    Stream.publish(eventName, payload);
+  }
+
+  static listen<T>(eventName: EventTypeEnum[]) {
+    return Stream.asyncIterator<T>(eventName);
+  }
+}
