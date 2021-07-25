d24371f002bc6da283ccc8aea2fb7dadf830c178
Borja Zarco <borja@squaads.com>
2021-07-25 20:24:33 +0100

    add created at timestamp

--- src/repostory/mongo/entitites/event.entity.ts
+++ src/repostory/mongo/entitites/event.entity.ts
@@ -1,9 +1,12 @@
 import { model, Schema } from 'mongoose';
 import { EventTypeEnum } from '../../../core/event-store/event-type.enum';
 
-const EventSchema = new Schema({
-  type: { type: String, enum: [...Object.values(EventTypeEnum)] },
-  data: { type: Schema.Types.Mixed },
-});
+const EventSchema = new Schema(
+  {
+    type: { type: String, enum: [...Object.values(EventTypeEnum)] },
+    data: { type: Schema.Types.Mixed },
+  },
+  { timestamps: { updatedAt: false } }
+);
 
 export const EventEntity = model<Event>('event-store', EventSchema, 'event-store');
