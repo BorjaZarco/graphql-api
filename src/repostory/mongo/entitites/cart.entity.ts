35a43d6be8e27fc452eeffb11f15fa3a8876d8df
Borja Zarco <borja@squaads.com>
2021-07-27 16:14:53 +0100

    update address entity

--- src/repostory/mongo/entitites/cart.entity.ts
+++ src/repostory/mongo/entitites/cart.entity.ts
@@ -3,7 +3,7 @@ import { Cart } from '../../../graphql/cart/dtos/cart.dto';
 import { ItemSchema } from './item.entity';
 
 const CartSchema = new Schema({
-  userId: { type: String },
+  userId: { type: String, required: [true, 'The user id is required'], unique: [true, 'Each user must have only one cart'] },
   address: { type: String },
   totalPrice: { type: Number },
   items: [ItemSchema],
