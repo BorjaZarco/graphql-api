fefe14e604e8bf6f32cebefb1ef1e717724e6d57
Borja Zarco <borja@squaads.com>
2021-07-27 13:31:15 +0100

    add stronger verification

--- src/graphql/user/user.resolver.ts
+++ src/graphql/user/user.resolver.ts
@@ -2,6 +2,7 @@ import bycript from 'bcrypt';
 import dotenv from 'dotenv';
 import { Arg, Mutation, Resolver } from 'type-graphql';
 import { AuthService } from '../../services/auth.service';
+import { UtilsService } from '../../services/utils.service';
 import { EmailInput } from './dtos/user.dto';
 import { UserModel } from './user.model';
 
@@ -12,6 +13,13 @@ const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS as string) || 10;
 export class UserResolver {
   @Mutation(() => String)
   async signUp(@Arg('data', () => EmailInput) { email, password }: EmailInput) {
+    if (!UtilsService.validateEmail(email)) {
+      throw new Error('Email not valid');
+    }
+    if ((password || '').length < 6) {
+      throw new Error('Password not valid, it should consist of 6 or more characters');
+    }
+
     const user = await UserModel.getUserByEmail(email);
     if (user) {
       throw new Error('Email already in use');
@@ -23,9 +31,12 @@ export class UserResolver {
 
   @Mutation(() => String)
   async logIn(@Arg('data', () => EmailInput) { email, password }: EmailInput) {
+    if (!email) throw new Error('Email string is required');
+    if (!password) throw new Error('Password string is required');
+
     const user = await UserModel.getUserByEmail(email);
     if (!user) {
-      throw new Error('User not found');
+      throw new Error('User with provided email not found');
     }
 
     if (!bycript.compareSync(password, user.password)) {
